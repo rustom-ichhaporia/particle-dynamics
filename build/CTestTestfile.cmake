16e0b316d0aaa507ed53082f25f5831f86225d63
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2020-10-27 18:53:15 -0500

    Create CMake framework

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/rustomichhaporia/GitHub/Cinder/my-projects/ideal-gas-rustom-ichhaporia
-# Build directory: /Users/rustomichhaporia/GitHub/Cinder/my-projects/ideal-gas-rustom-ichhaporia/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
